5fb7c75e4f07e1a41dcfbc278da4b16741ae0cca
Thirukumaran K <thirukumaranece@yahoo.com>
2021-07-31 14:29:13 +0530

    Docker React Initial First Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Hi Docker React Updated with Kubernetes
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Docker
         </a>
       </header>
     </div>
